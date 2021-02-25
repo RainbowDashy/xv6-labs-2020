56a35e7fa3e614a6079e76547963f0bf66cb88de
RainbowDashy <907577610@qq.com>
2021-02-25 04:11:24 -0800

    delete debug info

--- kernel/fs.c
+++ kernel/fs.c
@@ -311,17 +311,11 @@ ilock(struct inode *ip)
       panic("ilock: no type");
   }
 }
-
-void m4d3() {
-  printf("made!\n");
-}
-
 // Unlock the given inode.
 void
 iunlock(struct inode *ip)
 {
   if(ip == 0 || !holdingsleep(&ip->lock) || ip->ref < 1)
-    m4d3(),
     panic("iunlock");
 
   releasesleep(&ip->lock);
